5cefe00c002230effea40be6a577f31d779d9516
Therese Relucio <trelucio@udallas.edu>
2021-11-03 23:27:15 -0500

    working widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,27 +1,22 @@
-// Widget Test
-// Tests to see if grid box tap is working
-// 
+// Widget Tests
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:tic_tac_toe/main.dart';
 
 void main() {
-  testWidgets('Restart checker', (WidgetTester tester) async {
+  testWidgets('verify widgets', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
 
-    // // Verify that our counter starts at 0.
-    // expect(find.text('0'), findsOneWidget);
-    // expect(find.text('1'), findsNothing);
+    // Verify that certain words do show up and some don't 
+    expect(find.text('Tic-Tac-Toe'), findsOneWidget);
+    expect(find.text('Start Game'), findsOneWidget);
+    expect(find.text('foo'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.restart_alt));
+    // Verify there's a start button to start the game
+    await tester.tap(find.byType(ElevatedButton));
     await tester.pump();
-
-    // // Verify that our counter has incremented.
-    // expect(find.text('0'), findsNothing);
-    // expect(find.text('1'), findsOneWidget);
+    
   });
 }
